f5ddd8f9207c0de9f2aecc6e3ac1bf784304038f
Syfe <22247810+ItsSyfe@users.noreply.github.com>
2022-01-28 19:28:13 +0000

    feat: refactored, implemented randomness and infinite beeps
    Implemented Randomizer.cs and AudioManger.cs
    
    - Infinitely runs
    - Randomly beeps
    - Random volume for beeps
    - Random amount of beeps
    - Hidden from task bar (only visible in task manager)
    
    Signed-off-by: Syfe <22247810+ItsSyfe@users.noreply.github.com>

---
+++ BeepBoop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace BeepBoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Randomizer randomizer = new Randomizer();
+
+            WindowManager.hideWindow();
+
+            while (true)
+            {
+
+                int amountOfBeeps = randomizer.GetAmountOfBeeps();
+                
+                for (int i = 0; i < amountOfBeeps; i++)
+                {
+                    AudioManager.SetMasterVolume(randomizer.GetRandomVolume());
+                    Console.Beep();
+                }
+                
+                Thread.Sleep(randomizer.GetBeepTimeout());
+
+            }
+        }
+    }
+}
---
+++ BeepBoop/Randomizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BeepBoop
+{
+    public class Randomizer
+    {
+        private Random random = new Random();
+
+        public int GetBeepTimeout()
+        {
+            return random.Next(0, 60) * 1000;
+        }
+
+        public int GetAmountOfBeeps()
+        {
+            return random.Next(0, 8);
+        }
+
+        public float GetRandomVolume()
+        {
+            return (float) random.Next(0, 100);
+        }
+    }
+}
